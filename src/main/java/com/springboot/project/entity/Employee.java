240c3fa79ef880ec2a79d7fab4de057ec0a8e154
harshil-padasala <harshilpadsala999@gmail.com>
2023-01-24 21:35:36 +0530

    added Employee class in entity package and EmployeeRepository interface in repository package

---
+++ src/main/java/com/springboot/project/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.springboot.project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String emailId;
+
+    private Integer empId;
+
+}
